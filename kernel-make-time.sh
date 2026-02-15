cfb772826672bf351fb4ee8cc95889e8ab59d159
a-sik <andreyskor07@gmail.com>
2026-02-15 14:25:51 +0300

    remove make's message deleting

--- kernel-make-time.sh
+++ kernel-make-time.sh
@@ -3,9 +3,9 @@
 N=$( grep -c processor /proc/cpuinfo )
 echo "threads | real time" > "./times_result.txt"
 for i in $( seq 0 $((2*N+1)) ); do
-    make clean > /dev/null 2>&1
-    make defconfig > /dev/null 2>&1
+    make clean 2>&1
+    make defconfig 2>&1
     echo "Start \'make -j$i\'"
-    real_time=$({ time make -j$i > /dev/null 2>&1; } | grep real | awk '{print $2}') 
+    real_time=$({ time make -j$i 2>&1; } | grep real | awk '{print $2}') 
     echo " $i | $real_time" >> "./times_result.txt"
 done
