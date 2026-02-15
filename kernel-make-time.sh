0835987a0554f3903b5708e6b584dbf91192e048
a-sik <andreyskor07@gmail.com>
2026-02-15 13:45:04 +0300

    base make script

---
+++ kernel-make-time.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+N=$( grep -c processor /proc/cpuinfo )
+echo "threads | real time" > "times_result.txt"
+for i in $( seq 0 $((2*N+1)) ); do
+    make clean > /dev/null 2>&1
+    make defconfig > /dev/null 2>&1
+
+    real_time=$({ time make -j$i > /dev/null 2>&1; } 2>&1 | grep real | awk '{print $2}') 
+    echo " $i | $real_time" >> "times_result.txt"
+done
